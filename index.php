ee9cfb69ceee479f13815c064315eabb91d8c49d
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-10-11 22:29:53 +0000

    Fixed Folder names

--- index.php
+++ index.php
@@ -10,19 +10,27 @@
 </head>
 <body>
 
-        <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
-                <div class="carousel-inner">
-                  <div class="carousel-item active">
-                    <img class="d-block w-100 center-fit" src="img/slide1.jpg">
-                  </div>
-                  <div class="carousel-item">
-                    <img class="d-block w-100 center-fit" src="img/slide2.jpg">
-                  </div>
-                  <div classs="carousel-item">
-                    <img class="d-block w-100 center-fit" src="img/slide3.jpg">
-                  </div>
-                </div>
-              </div>
+<
+    <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img class="d-block w-100 center-fit" src="img/slide1.jpg">
+            </div>
+            <div class="carousel-item">
+                <img class="d-block w-100 center-fit" src="img/slide2.jpg">
+            </div>
+            <div classs="carousel-item">
+                <img class="d-block w-100 center-fit" src="img/slide3.jpg">
+            </div>
+        </div>
+    </div>
+
+    <div id="enterForm">
+    <form action="login.php">
+        <button type="submit">Click me to Login</button>
+        <button type="submit">Click me to Register</button>
+    </form>
+    </div>
 
 </body>
 </html>
