0229d8f425eab4e2a64f69a45ac0e204136c253c
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-10-11 22:21:00 +0000

    Fixed Slideshow

--- index.php
+++ index.php
@@ -2,18 +2,27 @@
 <html>
 <head> 
     <title>FlickTick</title>
-    <link rel="stylesheet" type="text/css" href="index.css">
-    <script type="text/javascript" src="index.js"></script>#
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel = "stylesheet" type = "text/css" href = "css/index.css"/>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
 <body>
 
+        <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100 center-fit" src="img/slide1.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100 center-fit" src="img/slide2.jpg">
+                  </div>
+                  <div classs="carousel-item">
+                    <img class="d-block w-100 center-fit" src="img/slide3.jpg">
+                  </div>
+                </div>
+              </div>
 
-    <div id="enterForm">
-    <form action="login.php">
-        <button type="submit">Click me to Login</button>
-        <button type="submit">Click me to Register</button>
-    </form>
-    </div>
 </body>
 </html>
