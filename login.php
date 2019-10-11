45b857a6c83afb7634416f3d0856049917085b04
GoodieDawson <46813327+GoodieDawson@users.noreply.github.com>
2019-10-11 20:46:57 +0000

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FlickTick</title>
+    <form action="login.php">
+        <input type="text" id="uname" name="uname">
+
+        <label>Password</label>
+        <input type="password" id="upass" name="upass">
+
+        <input type="submit" value="Login" name="lclicked">
+    </form>
+</head>
+<body>
+
+</body>
+</html>
