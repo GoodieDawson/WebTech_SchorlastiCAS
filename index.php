522af5adb50b276c904c7a508d1195b49b80323f
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-10-11 21:13:12 +0000

    Placed appropriate files in appropriate folders

--- index.php
+++ index.php
@@ -3,45 +3,11 @@
 <head> 
     <title>FlickTick</title>
     <link rel="stylesheet" type="text/css" href="index.css">
-    <script type="text/javascript" src="index.js"></script>
+    <script type="text/javascript" src="index.js"></script>#
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    
 
-    <!-- Slideshow container -->
-    <div class="slideshow-container">
-
-    <!-- Full-width images with number and caption text -->
-    <div class="mySlides fade">
-    <div class="numbertext">1 / 3</div>
-    <img src="C:\xampp\htdocs\Group_Project\slide1.jpg" style="width:100%">
-    <div class="text">Caption Text</div>
-    </div>
-
-    <div class="mySlides fade">
-    <div class="numbertext">2 / 3</div>
-    <img src="C:\xampp\htdocs\Group_Project\slide2.jpg" style="width:100%">
-    <div class="text">Caption Two</div>
-    </div>
-
-    <div class="mySlides fade">
-    <div class="numbertext">3 / 3</div>
-    <img src="C:\xampp\htdocs\Group_Project\slide3.jpg" style="width:100%">
-    <div class="text">Caption Three</div>
-    </div>
-
-    <!-- Next and previous buttons -->
-    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
-    <a class="next" onclick="plusSlides(1)">&#10095;</a>
-    </div>
-    <br>
-
-    <!-- The dots/circles -->
-    <div style="text-align:center">
-    <span class="dot" onclick="currentSlide(1)"></span>
-    <span class="dot" onclick="currentSlide(2)"></span>
-    <span class="dot" onclick="currentSlide(3)"></span>
-    </div>
 
     <div id="enterForm">
     <form action="login.php">
