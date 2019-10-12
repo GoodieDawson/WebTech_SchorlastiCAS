8c93505e2dadb7a34b1063c8fb7c1277943b1d95
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-10-12 02:24:51 +0000

    Altered backgriund and opacity of navbar

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
 <body>
     <header>
           <!--Navbar-->
-          <nav class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
+          <nav id="navbar" class="navbar navbar-expand-lg navbar-dark fixed-top scrolling-navbar">
             <div class="container">
               <a class="navbar-brand" href="#"><strong>FlickTick</strong></a>
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-7" aria-controls="navbarSupportedContent-7" aria-expanded="false" aria-label="Toggle navigation">
