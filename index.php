40c87e6d54eae3d542c8fc3834beda0ef0150220
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-10-12 00:26:06 +0000

    Updated Carousel on index page

--- index.php
+++ index.php
@@ -21,18 +21,18 @@
 <body>
 
 <div id="container">
-    <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000" style="z-index: 1; position: absolute;">
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img class="d-block w-100 center-fit" src="img/slide1.jpg">
-            </div>
-            <div class="carousel-item">
-                <img class="d-block w-100 center-fit" src="img/slide2.jpg">
-            </div>
-            <div classs="carousel-item">
-                <img class="d-block w-100 center-fit" src="img/slide3.jpg">
-            </div>
+    <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" style="z-index: 1; position: absolute;">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+        <img class="d-block w-100 center-fit" src="img/slide1.jpg">
         </div>
+        <div class="carousel-item">
+        <img class="d-block w-100 center-fit" src="img/slide2.jpg">
+        </div>
+        <div class="carousel-item">
+        <img class="d-block w-100 center-fit" src="img/slide3.jpg">
+        </div>
+    </div>
     </div>
 
     <div id="enterForm" style="z-index: 2; position: absolute;">
