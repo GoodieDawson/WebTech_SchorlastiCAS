1cc9712ed0b644b6ae8f2516c930f56c9590d835
Pamhie <46672542+Pamhie@users.noreply.github.com>
2019-10-12 03:23:04 +0000

    Add files via upload

---
+++ WebTech_SchorlastiCAS-master/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>FlickTick</title>
+    <link rel = "stylesheet" type = "text/css" href = "css/index.css"/>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet">
+</head>
+<body>
+
+        <div id="carouselExampleSlidesOnly" class="carousel slide carousel-fade" data-ride="carousel" data-interval="3000">
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img class="d-block w-100 center-fit" src="img/slide1.jpg">
+                  </div>
+                  <div class="carousel-item">
+                    <img class="d-block w-100 center-fit" src="img/slide2.jpg">
+                  </div>
+                  <div classs="carousel-item">
+                    <img class="d-block w-100 center-fit" src="img/slide3.jpg">
+                  </div>
+                </div>
+              </div>
+
+</body>
+</html>
