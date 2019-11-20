5ac839be3c0ef84f958268f605006709e2f8f121
GoodieDawson <goodie.dawson@ashesi.edu.gh>
2019-11-20 20:56:24 +0000

    Added validation checks to registration among other changes

--- db/user.php
+++ db/user.php
@@ -9,13 +9,16 @@
         private $last_name = null;
         private $gender = null;
         private $date_of_birth = null;
+        private $nationality = null;
         private $address = null;
         private $user_email = null;
         private $contact_number = null;
-        private $create_query = "INSERT INTO user (User_Name, User_Password, First_Name, Last_Name, Gender, Date_of_Birth, Address, Email_Address, Contact_Number) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        private $create_query = "INSERT INTO user (User_Name, User_Password, First_Name, Last_Name, Gender, Date_of_Birth, Nationality, Address, Email_Address, Contact_Number) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         private $select_query = "SELECT * FROM user WHERE User_Name = ? AND User_Password = ?";
+        private $select_query_byusername = "SELECT * FROM user WHERE User_Name = ?";
+        private $select_query_byemail = "SELECT * FROM user WHERE Email_Address = ?";
 
-        function __construct($user_name='none', $user_password='none', $first_name='none', $last_name='none', $gender='none', $date_of_birth='none', $address='none', $user_email='none', $contact_number='none'){
+        function __construct($user_name='none', $user_password='none', $first_name='none', $last_name='none', $gender='none', $date_of_birth='none', $nationality='none', $address='none', $user_email='none', $contact_number='none'){
             parent::__construct();
 			$this->user_name = $user_name;
             $this->user_password = $user_password;
@@ -23,6 +26,7 @@
             $this->last_name = $last_name;
             $this->gender = $gender;
             $this->date_of_birth = $date_of_birth;
+            $this->nationality =$nationality;
             $this->address = $address;
             $this->user_email = $user_email;
             $this->contact_number = $contact_number;
@@ -34,7 +38,7 @@
 
             if ($query_statement) {
                 
-                $query_statement->bind_param("sssssssss", $this->user_name, $this->user_password, $this->first_name, $this->last_name, $this->gender, $this->date_of_birth, $this->address, $this->user_email, $this->contact_number);
+                $query_statement->bind_param("ssssssssss", $this->user_name, $this->user_password, $this->first_name, $this->last_name, $this->gender, $this->date_of_birth, $this->nationality, $this->address, $this->user_email, $this->contact_number);
                 
                 $ret_val = $query_statement->execute();
                 
@@ -68,8 +72,54 @@
                 return "Error during selection";
             }
 
+        }
+
+        function get_user_byusername($user_name) {
+            $query_statement = mysqli_prepare($this->connection, $this->select_query_byusername);
+
+            if ($query_statement) {
+
+                $query_statement->bind_param("s", $user_name);
+
+                $query_statement->execute();
+
+                $ret_val = $query_statement->get_result();
+
+                $ret_val = $ret_val->num_rows;
+
+                $query_statement->close();
+
+                return $ret_val;
+            } else {
+                return "Error during selection";
+            }
+
 
         }
+
+        function get_user_byemail($user_email) {
+            $query_statement = mysqli_prepare($this->connection, $this->select_query_byemail);
+
+            if ($query_statement) {
+
+                $query_statement->bind_param("s", $user_email);
+
+                $query_statement->execute();
+
+                $ret_val = $query_statement->get_result();
+
+                $ret_val = $ret_val->num_rows;
+
+                $query_statement->close();
+
+                return $ret_val;
+            } else {
+                return "Error during selection";
+            }
+
+
+        }
+
     }
 
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+
+session_start();
+
+session_unset(); 
+
+session_destroy()
+
+?>
+
 <!DOCTYPE html>
 <html>
 
